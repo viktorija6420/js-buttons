3545c256ecef8a7f1deedbb14ce01a42ab297186
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-12-13 09:18:05 -0500

    added px line 7 and 8

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function moveButton(event){
   const button = event.target;
   const offsetLeft = Math.random() * (window.innerWidth - button.clientWidth);
   const offsetTop = Math.random()* (window.innerHeight - button.clientHeight);
-  button.style.top = offsetTop;
-  button.style.left = offsetLeft;
+  button.style.top = offsetTop + 'px';
+  button.style.left = offsetLeft + 'px';
 };
 
 crazyButtons.forEach(button => button.addEventListener('mouseenter', moveButton));
